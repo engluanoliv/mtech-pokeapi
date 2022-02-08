b4c7f57f7b654b4c6b8c2817299cf5f313b1d506
luanoliveiradev <luanoliveira.dev@incibrasil.com.br>
2022-02-08 18:15:10 -0300

    some changes are done

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from '../../Services/api';
 import { InputPokemon } from "./style";
 
 const InputSearch = () => {
 
-    const [searchedPokemon, setSearchedPokemon] = useState([]);
+    // const [searchedPokemon, setSearchedPokemon] = useState([]);
 
-    const searchPokemon = (e) => {
 
-        setSearchedPokemon(e.target.value);
 
-    };
+    useEffect(() => {
 
-    
-    
+        const searchPokemon = async (e) => {
+            const res = await api(`/${e.target.value}`)
+            // setSearchedPokemon(e.target.value)
+            console.log(res)
+        };
+        searchPokemon();
+    }, [])
+
+
     return (
         <>
-            <InputPokemon placeholder="Busque aqui o seu Pokemón" onChange={searchPokemon} />
+            <InputPokemon placeholder="Busque aqui o seu Pokemón" onChange={this.searchPokemon} />
         </>
     );
 }
